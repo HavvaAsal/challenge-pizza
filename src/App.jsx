70f3c1ceba6b4a1892e73ec8751a761990c21104
HavvaAsal <havvasal20@gmail.com>
2024-11-05 17:39:33 +0300

    header jsx ve css

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <header>
+          <div className='header-container'>
+            <h1>Teknolojik Yemekler</h1>
+            <p> Anasayfa - Seçenekler - Sipariş Oluştur</p>
+          </div>
+        </header>
+       
       </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
+     
+      
     </>
   )
 }
