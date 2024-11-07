35f57a24310df2034cceb95e40eda419ab6bcc38
HavvaAsal <havvasal20@gmail.com>
2024-11-07 19:07:55 +0300

    validation ekler ve butonu aktifleştirir

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,133 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Form, FormGroup, Label, Input, ButtonGroup, Button } from 'reactstrap'
+import React from 'react';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
 import './App.css'
+import axios from 'axios';
+
 
 
 const extras = [
   "Pepperoni", "Sosis", "Kanada Jambonu", "Tavuk Izgara", "Soğan", "Domates", "Mısır", "Sucuk", "Jalepeno", "Sarımsak", "Biber", "Ananas", "Kabak"];
 const sizes = ["küçük", "orta", "büyük"];
-const hamur = ["ince", "orta", "kalın"]; 
+const hamur = ["ince", "orta", "kalın"];
 const initialForm = {
-  "boyut":"-1",
-  "hamur":"-1",
-  "malzeme":"[]",
+  "boyut": "-1",
+  "hamur": "-1",
+  "malzeme": [],
+  "note": ""
 }
 
 
 function App() {
+  const [formData, setFormData] = useState(initialForm);
+  const [errors, setErrors] = useState([]);
+  const [extraList, setExtraList] = useState([]);
+  const [sayi, setSayi] = useState(1);
+  const [isValid, setİsValid] = useState(false);
+  const [secimler, setSecimler] = useState(0);
+  const [toplam, setToplam] = useState();
+
+  const arttir = (() => {
+    setSayi(sayi + 1)
+  });
+
+  const azalt = (() => {
+    if (sayi > 1) {
+      setSayi(sayi - 1)
+    }
+  })
+
+  const secim = (() => {
+    if (extraList.length > 4 && extraList.length < 10) {
+      setSecimler(secimler + (extraList.length * 5))
+    }
+  })
+
+  const toplamHesap = (() => {
+    setToplam(toplam + secimler)
+  })
+
+  const handleChange = (event) => {
+    const { name, checked, type, value } = event.target
+    console.log("type, name, value, checked:", type, name, value, checked)
 
 
-  const [squares, setSquares] = useState([]);
+    if (type === "checkbox") {
+      if (checked) {
+        setExtraList([...extraList.value]);
+      } else {
+        setExtraList(extraList.filter((item) => item !== value));
+      }
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
 
-  const handleSquareChange = (index) => {
-    setSquares((squares) =>
-      squares.includes(index)
-        ? squares.filter((i) => i !== index)
-        : [...squares, index]
-    );
+
+    if (name === "boyut") {
+      if (value === "-1") {
+        setErrors({ ...errors, boyut: "Boyut seçiniz." })
+      } else {
+        setErrors({ ...errors, boyut: "" })
+      }
+    }
+
+    if (name === "hamur") {
+      if (value === "-1") {
+        setErrors({ ...errors, hamur: "Hamur kalınlığını seçiniz." })
+      } else {
+        setErrors({ ...errors, hamur: "" });
+      }
+    }
+
+    if (name === "malzeme" && checked === true) {
+      setExtraList([...extraList, "yenimalzeme"]);
+    }
+    console.log(extraList.length);
+
+    if (name === "malzeme")
+      if (extraList.length < 4 || extraList.length > 10) {
+        setErrors({ ...errors, malzeme: "En az 4 tane en fazla 10 tane malzeme seçiniz." })
+      } else {
+        setErrors({ ...errors, malzeme: "" })
+      }
+
+    if (name === "note")
+      if (value.length < 3) {
+        setErrors({ ...errors, note: "En az 3 karakter giriniz." })
+      } else {
+        setErrors({ ...errors, note: "" })
+      }
+
+    setErrors({ ...errors })
+    console.log(setErrors({ ...errors }))
+  }
+
+  useEffect(() => {
+    if ((formData.boyut && !errors.boyut) &&
+      (formData.hamur && !errors.hamur) &&
+      (formData.malzeme && !errors.malzeme) &&
+      (formData.note && !errors.note)) {
+      setİsValid(true)
+    } else {
+      setİsValid(false)
+    }
+  }, [formData, errors]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (!isValid) return
+
+    axios
+      .post("https://reqres.in/api/pizza", formData)
+      .then((response) => {
+        setFormData(initialForm)
+        console.log("Sipariş Özeti:", response.data);
+      })
+      .catch((error) => {
+        console.error("Hata Oluştu:".error);
+      });
   };
 
   return (
@@ -57,79 +156,111 @@ function App() {
               </p>
             </div>
           </div>
+
           <div className="size-selection">
-            <h2>Boyut Seç<span style={{ color: "red" }}>*</span></h2>
-            <label>
-              <input type="radio" name="size" value="small" />
-              Küçük
-            </label>
-            <label>
-              <input type="radio" name="size" value="medium" />
-              Orta
-            </label>
-            <label>
-              <input type="radio" name="size" value="large" />
-              Büyük
-            </label>
-          </div>
-          <div className="size-selection">
-            <h2>Hamur Seç<span style={{ color: "red" }}>*</span></h2>
-            <label for="type-select"></label>
-            <select id="type-select" name="type">
-              <option value="" selected disabled>Hamur Kalınlığı</option>
-              <option value="thin">İnce</option>
-              <option value="medium">Orta</option>
-              <option value="thick">Kalın</option>
-            </select>
-          </div>
-          <div className="square-selection">
-            <h2>Ek Malzemeler</h2>
-            <p>En fazla 10 malzeme seçebilirsiniz. 5₺</p>
-            <div className="squares-container">
-              {extras.map((extra, index) => (
-                <label key={index} className="square">
-                  <input
-                    type="checkbox"
-                    checked={squares.includes(index)}
-                    onChange={() => handleSquareChange(index)} />
-                  <span className={squares.includes(index) ? 'selected' : ''}>
-                    {extra}
-                  </span>
-                </label>
-              ))}
-            </div>
-          </div>
-          <Form>
-            <FormGroup>
-              <Label for="exampleText">
-                <h2>Sipariş Notu</h2>
+            <Form className='form-group'>
+
+              <Label
+                className='size'
+                htmlFor='boyut'>Boyut Seç<span style={{ color: "red" }}>*</span>
               </Label>
+
+              {sizes.map((boyut, index) => {
+                return <FormGroup>
+                  <Input
+                    key={index}
+                    id={boyut}
+                    name="boyut"
+                    type="radio"
+                    onChange={handleChange}
+                    value={formData.boyut} />{" "}
+                  <Label htmlFor={boyut}>{boyut}</Label>
+                </FormGroup>
+              })}
+            </Form>
+          </div>
+
+          <div className="size-selection">
+            <Form className='form-group'>
+              <Label
+                className="size"
+                htmlFor="hamur">Hamur Seç<span style={{ color: "red" }}>*</span>
+              </Label>
+
+              <select onChange={handleChange}
+                value={formData.hamurlar} >
+                {hamur.map((hamur) => {
+                  return <option value={hamur}>{hamur}</option>
+                })}
+              </select>
+            </Form>
+          </div>
+
+          <div className="extra-selection">
+            <Form>
+
+              <h2>Ek Malzemeler</h2>
+              <p>En fazla 10 malzeme seçebilirsiniz. 5₺ <span style={{ color: "red" }}>*</span></p>
+              {extras.map((mlz, index) => {
+                return <FormGroup className='mlz' key={index}>
+                  <Input
+                    id={mlz}
+                    name="malzeme"
+                    type="checkbox"
+                    onChange={handleChange}
+                    checked={formData.mlz} />
+                  <Label htmlFor={mlz}>{mlz}</Label>
+                </FormGroup>
+              })}
+            </Form>
+          </div>
+
+          <div>
+            <FormGroup>
+              <Label className="text">Sipariş Notu</Label>
               <Input
-                id="exampleText"
-                name="text"
+                id="note"
+                name="note"
                 type="textarea"
                 placeholder='Siparişinize eklemek istediğiniz bir not var mı?'
+                onChange={handleChange}
               />
             </FormGroup>
-          </Form>
+          </div>
 
           <hr></hr>
 
           <ButtonGroup>
-            <Button className="yellow">
+            <div className='btn'></div>
+            <Button onClick={azalt} className='eksi'>
               -
             </Button>
-            <Button className="white">
-              1
-            </Button>
-            <Button className="yellow">
+            <Button className='sayi'>{sayi} </Button>
+            <Button onClick={arttir} className='arti'>
               +
             </Button>
           </ButtonGroup>
-        </main>
 
+          <div className='toplam'>
+            <h2>Sipariş Toplamı</h2>
+            <div>
+              <p>Seçimler</p>
+              <p>{secim}₺</p>
+            </div>
 
+            <div>
+              <p>Toplam</p>
+              <p>{toplamHesap}₺</p>
+            </div>
+          </div>
+          <div>
+            <Form onSubmit={handleSubmit}>
+              <Button disabled={!isValid} type="submit" className="siparisVer">SİPARİŞ VER</Button>
+            </Form>
+          </div>
+        </main >
       </div>
+
 
     </>
   )
