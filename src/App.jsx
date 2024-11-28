dd97bb8f1563bb09180de0366578870df2497a8b
HavvaAsal <havvasal20@gmail.com>
2024-11-28 09:46:46 +0300

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { useEffect, useState } from 'react'
 import { Form, FormGroup, Label, Input, ButtonGroup, Button } from 'reactstrap'
-import React from 'react';
+import axios from 'axios';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import axios from 'axios';
 
-
-
-const extras = [
-  "Pepperoni", "Sosis", "Kanada Jambonu", "Tavuk Izgara", "Soğan", "Domates", "Mısır", "Sucuk", "Jalepeno", "Sarımsak", "Biber", "Ananas", "Kabak"];
-const sizes = ["küçük", "orta", "büyük"];
-const hamur = ["ince", "orta", "kalın"];
+const extrasList = ["Pepperoni", "Sosis", "Kanada Jambonu", "Tavuk Izgara", "Soğan", "Domates", "Mısır", "Sucuk", "Jalepeno", "Sarımsak", "Biber", "Ananas", "Kabak"];
+const boyutList = ["küçük", "orta", "büyük"];
+const doughList = ["ince", "normal", "kalın"];
 const initialForm = {
   "boyut": "-1",
   "hamur": "-1",
-  "malzeme": [],
+  "extras": [],
   "note": ""
 }
 
@@ -23,81 +19,78 @@ const initialForm = {
 function App() {
   const [formData, setFormData] = useState(initialForm);
   const [errors, setErrors] = useState([]);
-  const [extraList, setExtraList] = useState([]);
   const [sayi, setSayi] = useState(1);
-  const [isValid, setİsValid] = useState(false);
-  const [secimler, setSecimler] = useState(0);
-  const [toplam, setToplam] = useState();
+  const [isValid, setIsValid] = useState(false);
 
-  const arttir = (() => {
+  const arttir = () => {
     setSayi(sayi + 1)
-  });
+  };
 
-  const azalt = (() => {
+  const azalt = () => {
     if (sayi > 1) {
       setSayi(sayi - 1)
     }
-  })
+  }
+  const secim = formData.extras.length > 4 && formData.extras.length < 10
+    ? formData.extras.length * 5
+    : 0;
 
-  const secim = (() => {
-    if (extraList.length > 4 && extraList.length < 10) {
-      setSecimler(secimler + (extraList.length * 5))
-    }
-  })
-
-  const toplamHesap = (() => {
-    setToplam(toplam + secimler)
-  })
-
+  const toplamHesap = secim + (sayi * 85.5);
+  
   const handleChange = (event) => {
     const { name, checked, type, value } = event.target
     console.log("type, name, value, checked:", type, name, value, checked)
 
-
-    if (type === "checkbox") {
-      if (checked) {
-        setExtraList([...extraList.value]);
+    if (name === "malzeme") {
+      if (checked === true) {
+        setFormData({
+          ...formData,
+          extras: [...formData.extras, value]
+        });
       } else {
-        setExtraList(extraList.filter((item) => item !== value));
+        setFormData({
+          ...formData,
+          extras: formData.extras.filter((ext) => ext !== value)
+        });
       }
     } else {
       setFormData({ ...formData, [name]: value });
     }
 
 
+
+    // validasyonlar
+
     if (name === "boyut") {
-      if (value === "-1") {
-        setErrors({ ...errors, boyut: "Boyut seçiniz." })
+      if (value === "") {
+        setErrors({ ...errors, [name]: "Boyut seçiniz." })
       } else {
-        setErrors({ ...errors, boyut: "" })
+        setErrors({ ...errors, [name]: "" })
       }
     }
 
     if (name === "hamur") {
-      if (value === "-1") {
-        setErrors({ ...errors, hamur: "Hamur kalınlığını seçiniz." })
+      if (value === "") {
+        setErrors({ ...errors, [name]: "Hamur kalınlığını seçiniz." })
       } else {
-        setErrors({ ...errors, hamur: "" });
+        setErrors({ ...errors, [name]: { name } });
       }
     }
 
-    if (name === "malzeme" && checked === true) {
-      setExtraList([...extraList, "yenimalzeme"]);
-    }
-    console.log(extraList.length);
+
 
     if (name === "malzeme")
-      if (extraList.length < 4 || extraList.length > 10) {
-        setErrors({ ...errors, malzeme: "En az 4 tane en fazla 10 tane malzeme seçiniz." })
+      if (formData.extras.length < 4 || formData.extras.length > 10) {
+        setErrors({ ...errors, [name]: "En az 4 tane en fazla 10 tane malzeme seçiniz." })
       } else {
-        setErrors({ ...errors, malzeme: "" })
+        setErrors({ ...errors, [name]: "" })
       }
 
     if (name === "note")
-      if (value.length < 3) {
-        setErrors({ ...errors, note: "En az 3 karakter giriniz." })
+      if (formData.note.length < 3) {
+        setErrors({ ...errors, [name]: "En az 3 karakter giriniz." })
       } else {
-        setErrors({ ...errors, note: "" })
+        setErrors({ ...errors, [name]: { name } })
       }
 
     setErrors({ ...errors })
@@ -107,27 +100,27 @@ function App() {
   useEffect(() => {
     if ((formData.boyut && !errors.boyut) &&
       (formData.hamur && !errors.hamur) &&
-      (formData.malzeme && !errors.malzeme) &&
+      (formData.extras && !errors.malzeme) &&
       (formData.note && !errors.note)) {
-      setİsValid(true)
+      setIsValid(true)
     } else {
-      setİsValid(false)
+      setIsValid(false)
     }
-  }, [formData, errors]);
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault()
     if (!isValid) return
 
     axios
-      .post("https://reqres.in/api/pizza", formData)
+      .post("https://reqres.in/api/pizza")
       .then((response) => {
         setFormData(initialForm)
         console.log("Sipariş Özeti:", response.data);
       })
       .catch((error) => {
-        console.error("Hata Oluştu:".error);
-      });
+        console.log(error)
+      })
   };
 
   return (
@@ -157,65 +150,59 @@ function App() {
             </div>
           </div>
 
-          <div className="size-selection">
-            <Form className='form-group'>
-
+          <Form className='form-group'>
+            <div className="size-selection">
               <Label
                 className='size'
                 htmlFor='boyut'>Boyut Seç<span style={{ color: "red" }}>*</span>
               </Label>
+              {boyutList
+                .map((boyut, index) => {
+                  return <FormGroup key={"s" + index}>
 
-              {sizes.map((boyut, index) => {
-                return <FormGroup>
-                  <Input
-                    key={index}
-                    id={boyut}
-                    name="boyut"
-                    type="radio"
-                    onChange={handleChange}
-                    value={formData.boyut} />{" "}
-                  <Label htmlFor={boyut}>{boyut}</Label>
-                </FormGroup>
-              })}
-            </Form>
-          </div>
+                    <Input
+                      id={boyut}
+                      name="boyut"
+                      type="radio"
+                      onChange={handleChange}
+                      value={formData.boyut} />{" "}
+                    <Label htmlFor={boyut}>{boyut}</Label>
+                  </FormGroup>
+                })}
+            </div>
 
-          <div className="size-selection">
-            <Form className='form-group'>
+            <div className="size-selection">
               <Label
                 className="size"
                 htmlFor="hamur">Hamur Seç<span style={{ color: "red" }}>*</span>
               </Label>
 
               <select onChange={handleChange}
-                value={formData.hamurlar} >
-                {hamur.map((hamur) => {
-                  return <option value={hamur}>{hamur}</option>
+                value={formData.hamur} >
+                {doughList.map((d, ind) => {
+                  return <option key={ind} value={d}>{d}</option>
                 })}
               </select>
-            </Form>
-          </div>
+            </div>
 
-          <div className="extra-selection">
-            <Form>
+            <div className="extra-selection">
 
               <h2>Ek Malzemeler</h2>
               <p>En fazla 10 malzeme seçebilirsiniz. 5₺ <span style={{ color: "red" }}>*</span></p>
-              {extras.map((mlz, index) => {
-                return <FormGroup className='mlz' key={index}>
+              {extrasList.map((ext, index) => {
+                return <FormGroup className='mlz' key={"ext" + index}>
                   <Input
-                    id={mlz}
+                    id={ext}
+                    value={ext}
                     name="malzeme"
                     type="checkbox"
                     onChange={handleChange}
-                    checked={formData.mlz} />
-                  <Label htmlFor={mlz}>{mlz}</Label>
+                    checked={formData.extras.includes(ext)} />
+                  <Label htmlFor={ext}>{ext}</Label>
                 </FormGroup>
               })}
-            </Form>
-          </div>
+            </div>
 
-          <div>
             <FormGroup>
               <Label className="text">Sipariş Notu</Label>
               <Input
@@ -226,38 +213,36 @@ function App() {
                 onChange={handleChange}
               />
             </FormGroup>
-          </div>
 
-          <hr></hr>
+            <hr></hr>
 
-          <ButtonGroup>
-            <div className='btn'></div>
-            <Button onClick={azalt} className='eksi'>
-              -
-            </Button>
-            <Button className='sayi'>{sayi} </Button>
-            <Button onClick={arttir} className='arti'>
-              +
-            </Button>
-          </ButtonGroup>
+            <ButtonGroup>
+              <div className='btn'></div>
+              <Button onClick={azalt} className='eksi'>
+                -
+              </Button>
+              <div className='sayi'>{sayi}</div>
+              <Button onClick={arttir} className='arti'>
+                +
+              </Button>
+            </ButtonGroup>
 
-          <div className='toplam'>
-            <h2>Sipariş Toplamı</h2>
-            <div>
-              <p>Seçimler</p>
-              <p>{secim}₺</p>
+            <div className='toplam'>
+              <h2>Sipariş Toplamı</h2>
+              <div>
+                <p>Seçimler</p>
+                <p>{secim}₺</p>
+              </div>
+
+              <div>
+                <p>Toplam</p>
+                <p>{toplamHesap}₺</p>
+              </div>
             </div>
-
             <div>
-              <p>Toplam</p>
-              <p>{toplamHesap}₺</p>
-            </div>
-          </div>
-          <div>
-            <Form onSubmit={handleSubmit}>
               <Button disabled={!isValid} type="submit" className="siparisVer">SİPARİŞ VER</Button>
-            </Form>
-          </div>
+            </div>
+          </Form>
         </main >
       </div>
 
